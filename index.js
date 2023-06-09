3325d3f0e99755c998c309c58eaae92c93729644
Puneet Kumar Singh <puneetkumarsingh10102003@gmail.com>
2023-06-09 08:54:02 +0530

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+const port=8000;
+app.listen(port,function(err){
+    if(err){
+        console.log(`error is ${err}`);
+        return;
+    }
+    else{
+        console.log("server is running fine");
+    }
+
+})
