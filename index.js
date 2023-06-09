ff9cfb5164fe914e208f74c8a4c7a4ece3d2fd4d
CaptainLevi101 <200104016@hbtu.ac.in>
2023-06-09 13:08:14 +0530

    add a controller+router+view

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const app=express();
 const port=8000;
+//use express router
+app.use('/',require('./routes/index'));
+app.set('view engine', 'ejs');
 app.listen(port,function(err){
     if(err){
         console.log(`error is ${err}`);
