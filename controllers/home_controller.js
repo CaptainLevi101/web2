a5c618e575cbb1ff3e068493fa4ff7cca5b0ca40
CaptainLevi101 <200104016@hbtu.ac.in>
2023-06-19 20:49:04 +0530

    updated

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,14 @@
-module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Home"
-    });
+const Post=require('../models/post');
+module.exports.home = async function(req, res){
+    try {
+        const posts = await Post.find({}).populate('user');
+        return res.render('home', {
+          title: "Home",
+          posts: posts
+        });
+      } catch (err) {
+        // Handle the error appropriately
+      }
+      
+   
 }
