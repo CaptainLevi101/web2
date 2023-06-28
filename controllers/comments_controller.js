94376e00ceb6ee57c89c4e69d1c15eeebd934125
CaptainLevi101 <200104016@hbtu.ac.in>
2023-06-28 10:23:38 +0530

    deleted comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -18,3 +18,25 @@ module.exports.create = async function(req, res){
         // Handle error
     }
 }
+module.exports.destroy = async function (req, res) {
+    try {
+      const comment = await Comment.findById(req.params.id).exec();
+      
+      if (comment.user == req.user.id) {
+        const postId = comment.post;
+  
+        await comment.deleteOne();
+  
+        await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }).exec();
+        
+        return res.redirect('back');
+      } else {
+        return res.redirect('back');
+      }
+    } catch (err) {
+      // Handle any errors that occurred during execution
+      console.error(err);
+      return res.redirect('back');
+    }
+  }
+  
